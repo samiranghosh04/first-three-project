c21cc60667e6213e102c84a0ad05dec87ad2cdc3
samiranghosh04 <62144424+samiranghosh04@users.noreply.github.com>
2023-03-06 22:07:31 +0530

    first build

--- main.js
+++ main.js
@@ -1,25 +1,26 @@
 import './style.css'
 import * as THREE from 'three';
-//import {gsap} from "gsap";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
+//Create a scene
 const scene = new THREE.Scene();
+
+//Create a Sphere
 const geometry= new THREE.SphereGeometry(3,64,64);
 const material=new THREE.MeshStandardMaterial({
     color:'#00ff83',
     roughness: 0.5,
 })
-
 const mesh= new THREE.Mesh(geometry, material);
 scene.add(mesh)
 
-//Sizes
+//Sizes the canvas
 const sizes={
     width:window.innerWidth,
     height:window.innerHeight,
 }
 
-//Light
+//Lights
 const light = new THREE.PointLight(0xffffff,1,100)
 light.position.set(0,10,10)
 light.intensity = 1.25
@@ -45,9 +46,7 @@ controls.enableZoom = false
 controls.autoRotate = true
 controls.autoRotateSpeed = 5
 
-
-
-//Resize
+//Resizes the canvas
 window.addEventListener('resize', () => {
     //Updating sizes
     sizes.width = window.innerWidth
@@ -64,18 +63,17 @@ const loop = () => {
 }
 loop()
 
-//Timeline
+//Timeline using GSAP 
 const tl=gsap.timeline({ defaults: { duration:1 } })
 tl.fromTo(mesh.scale, {z:0, x:0, y:0}, {z:1, x:1, y:1})
 tl.fromTo("nav", {y:"-100%"}, {y:"0%"})
 tl.fromTo(".title", { opacity:0 }, { opacity:1 })
 
-//Mouse Animation
+//Mouse Animation using GSAP
 let mouseDown = false
 let rgb = []
 window.addEventListener("mousedown", ()=> (mouseDown =true))
 window.addEventListener("mouseup", ()=> (mouseDown =false))
-
 window.addEventListener("mousemove", (e) =>{
     if(mouseDown){
         rgb = [
